ed3f474506d70de679a9a186e1dd6bab76ee9b71
Patrick Hall <jpatrickhall@gmail.com>
2016-08-20 11:17:56 -0400

    Strawman dir structure for personal and user contributions

---
+++ py-sklearn/py-sklearn-models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 20 10:42:08 2016
+
+py-sklearn-models.py
+
+Will contain models from sklearn package
+
+Contributors will add data to appropriate folder and (concise, neat) modeling
+scripts to this file. All (concise, neat) modeling scripts must create a list
+with following elements:
+
+* model name
+* model description
+* data name
+* data description
+* performance
+
+This resulting list will be appended to the global models list at the end of
+each contributors script. The results in the global models list will be
+described automatically with a table and plot in the main repo README.md.
+
+"""
+
---
+++ r-h2o/r-h2o-models.r
@@ -0,0 +1,17 @@
+###############################################################################
+# r-h2o-models.r                                                              #
+# Will contain models from h2o.ai package using R interface                   #
+#                                                                             #
+# Contributors will add data to appropriate folder and (concise, neat)        #
+# modeling scripts to this file. All (concise, neat) modeling scripts must    #
+# create a list with following elements:                                      #
+#   model name                                                                #
+#   model description                                                         #
+#   data name                                                                 #
+#   data description                                                          #
+#   performance                                                               #
+#                                                                             #
+# This resulting list will be appended to the global models list at the end   #
+# of each contributors script. The results in the global models list will be  #
+# described automatically with a table and plot in the main repo README.md.   #
+###############################################################################
